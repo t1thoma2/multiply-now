9becc746cd31fba0694aabb24b45dc8cd748af33
Trenton Telge <5915803+trenton-telge@users.noreply.github.com>
2018-02-05 13:41:45 -0600

    Initial commit

---
+++ multiply.js
@@ -0,0 +1,119 @@
+var savedTime = 0;
+var timePerRound = 0;
+var rounds = 0;
+var gridX = 0;
+var gridY = 0;
+var max = 0;
+var xVal = new Array(gridX);
+var yVal = new Array(gridY);
+
+function startGame(difficulty){
+    switch (difficulty){
+        case 1:
+            gridX = 3;
+            gridY = 3;
+            timePerRound = 60;
+            max = 6;
+            break;
+        case 2:
+            gridX = 4;
+            gridY = 3;
+            timePerRound = 60;
+            max = 10;
+            break;
+        case 3:
+            gridX = 4;
+            gridY = 4;
+            timePerRound = 40;
+            max = 12;
+            break;
+        default:
+            break;
+    }
+    startSection()
+}
+function startSection() {
+    xVal = new Array(gridX);
+    yVal = new Array(gridY);
+    var i = 1;
+    while (i <= gridX){
+        xVal[i-1] = Math.floor(Math.random() * max) + 1;
+        i++;
+    }
+    var j = 1;
+    while (j <= gridY){
+        yVal[j-1] = Math.floor(Math.random() * max) + 1;
+        j++;
+    }
+    document.getElementById("postsection").style.display = "none";
+    document.getElementById("init").style.display = "none";
+    document.getElementById("game").style.display = "flex";
+    rounds++;
+    var tableHTML = "<div class='game-table'><div class='game-row'><p class='blank'></p>";
+    i = 1;
+    while (i <= gridX){
+        tableHTML = tableHTML + "<p class='game-header-x'>" + xVal[i-1].toString() + "</p>";
+        i++;
+    }
+    tableHTML = tableHTML + "</div>";
+
+    j = 1;
+    while (j <= gridY){
+        tableHTML = tableHTML + "<div class='game-row'>";
+        tableHTML = tableHTML + "<p class='game-header-y'>" + yVal[j-1].toString() + "</p>";
+        i = 1;
+        while (i <= gridX){
+            tableHTML = tableHTML + "<input class='game-input' id='" + i.toString() + "-" + j.toString() + "' type='number' />";
+            i++;
+        }
+        tableHTML = tableHTML + "</div>";
+        j++;
+    }
+
+    tableHTML = tableHTML + "<div class='game-row'><img class='fw-button done-button' onclick='finish();' src='img/done.svg' </div></div>";
+    document.getElementById("game").innerHTML = tableHTML;
+}
+
+function finish(){
+    var isIncorrect = false;
+
+    var j = 1;
+    while (j <= gridY) {
+        var i = 1;
+        while (i <= gridX){
+            console.log(roughScale(document.getElementById(i.toString() + "-" + j.toString()).value, 10)/100);
+            console.log((xVal[i-1]*yVal[j-1]));
+            console.log("------------------------");
+            if (roughScale(document.getElementById(i.toString() + "-" + j.toString()).value, 10)/100 !== (xVal[i-1]*yVal[j-1])){
+                isIncorrect = true;
+            }
+            i++;
+        }
+        j++;
+    }
+    if (!isIncorrect){
+        //TODO add remaining time
+
+        savedTime++;
+    }
+    document.getElementById("game").style.display = "none";
+    document.getElementById("postsection").style.display = "flex";
+    document.getElementById("score-total").innerHTML = savedTime.toString();
+}
+
+function levelUp(){
+    //TODO modify params
+    startSection()
+}
+
+function roughScale(x, base) {
+    var parsed = parseInt(x, base);
+    if (isNaN(parsed)) { return 0 }
+    return parsed * 100;
+}
+
+function review(){
+    //TODO action stub
+}
+
+//PSEUDO_RANDOM SEED TEACHER MODE
